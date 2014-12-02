b2f29027993fc79726c5be4f908a610d9f53fef6
Clint Moore <clint@ivy.io>
2014-12-01 18:07:25 -0800

    Now working.  lawl.

--- src/main/java/io/ivy/bnotifyListener.java
+++ src/main/java/io/ivy/bnotifyListener.java
@@ -1,16 +1,21 @@
 package io.ivy;
 
-import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ServerConnectedEvent;
 import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
 
-public class bnotifyListener implements Listener {
+public class bnotifyListener extends Plugin implements Listener {
     @EventHandler
     public void onServerConnected(final ServerConnectedEvent event) {
         String name = event.getServer().getInfo().getName();
-        event.getPlayer().sendMessage(new ComponentBuilder("FARTS INCLAALFJA").create());
-        //event.getPlayer().sendMessage(new ComponentBuilder("Welcome to " + event.getServer().getInfo().getName() + "!").color(ChatColor.GREEN).create());
+        
+        for (ProxiedPlayer target : getProxy().getPlayers()) {
+        	if (!name.equals(target.getName())) {
+        		target.sendMessage( new ComponentBuilder(name + " connected.").create());
+        	}
+        }
     }
 }
